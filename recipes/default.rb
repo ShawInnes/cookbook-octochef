21f489701576351ee4766b34f37195db06530337
Shaw Innes <shaw.innes@tattsgroup.com>
2016-05-14 00:10:26 +1000

    Add Reboot Handler

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,11 +6,18 @@
 
 sources = { '6c2c589132e830a185c5f40f82042bee3022e721a216680bd9b3995ba86f3781' => node['dotnet']['source'].to_s }
 
+reboot 'now' do
+  action :nothing
+  reason 'Cannot continue Chef run without a reboot.'
+  delay_mins 2
+end
+
 ms_dotnet_framework '4.5.2' do
   action            :install
   include_patches   true
   package_sources   sources
   require_support   true
+  notifies :reboot_now, 'reboot[now]', :immediately
 end
 
 package 'Seq' do
